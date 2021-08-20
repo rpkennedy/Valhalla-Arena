1b1663fcd85d2308ea738444bcad9f6e8768da62
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2021-08-20 12:52:25 -0400

    v0.2 - Creation / Start
    3D movement and look
    Select UI worked on

---
+++ Valhalla Arena/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 velocity;
+    
+    public float speed = 0.04f;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.right * x + transform.forward * z;
+
+        controller.Move(direction * speed);
+    }
+}
